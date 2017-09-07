579f40c454ef789ccfcb131fa584d0504e9b5994
Awe23123 <tszfungwonghk@gmail.com>
2017-09-07 17:14:14 +0800

    More stuff

--- JScratch/src/net/zr2/jscratch/sprite/Sprite.java
+++ JScratch/src/net/zr2/jscratch/sprite/Sprite.java
@@ -1,5 +1,42 @@
 package net.zr2.jscratch.sprite;
 
-public class Sprite {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import net.zr2.jscratch.event.Event;
 
+public class Sprite {
+	
+	protected final List<Costume> costumes = new ArrayList<>();
+	public final Map<Event, Runnable> eventHandlers = new HashMap<>();
+	
+	public final String name;
+	public double x = 0;
+	public double y = 0;
+	public double direction = 90;
+	
+	public int costumeNumber = 0;
+	public int size = 100;
+	
+	public double volume = 100;
+	public double videoMotion = -1;
+	
+	public Sprite(String name) {
+		this.name = name;
+	}
+	
+	public Costume getCurrentCostume() {
+		return this.costumes.get(this.costumeNumber);
+	}
+	
+	public boolean addCostume(Costume costume) {
+		if (this.costumes.contains(costume)) {
+			return false;
+		} else {
+			this.costumes.add(costume);
+			return true;
+		}
+	}
+	
 }
